738637bb151819c2e8029c2a7d8751ebb3c6d90d
kimeloo <149450798+kimeloo@users.noreply.github.com>
2025-01-14 09:48:48 +0900

    [D1] Title: 홀수만 더하기, Time: 107 ms, Memory: 41,940 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
@@ -1,7 +1,24 @@
-n = int(input())
-for i in range(n):
-    nums = list(map(int, input().split()))
-    total = 0
-    for num in nums:
-        total += num if num%2==1 else 0
-    print('#{} {}'.format(i+1, total))
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		int t, s, n;
+		Scanner sc = new Scanner(System.in);
+		t = sc.nextInt();
+		for (int tc=1; tc<=t; tc++) {
+			System.out.print("#"+tc+" ");
+			s = 0;
+			for (int i=0; i<10; i++) {
+				n = sc.nextInt();
+				if (n%2==1) {
+					s+=n;
+				}
+			}
+			System.out.println(s);
+		}
+		sc.close();
+
+	}
+
+}
