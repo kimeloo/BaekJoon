9f0f3751523e90deacd91e1f80c5ed5f2aa17da9
kimeloo <149450798+kimeloo@users.noreply.github.com>
2025-02-09 20:02:07 +0900

    [Bronze II] Title: Hashing, Time: 92 ms, Memory: 12780 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		int l;
-		long hash;
+		long hash, num;
 		Scanner sc = new Scanner(System.in);
-		l = sc.nextInt();
 		sc.nextLine();
 		char[] str = sc.nextLine().toCharArray();
 		sc.close();
 		
 		hash = 0;
+		num = 1;
 		for (int i=0; i<str.length; i++) {
-			hash += (str[i]-'a'+1)*Math.pow(31, i);
+			hash += (str[i]-'a'+1)*num;
+			num = (num*31)%1234567891L; 
 		}
 		System.out.println(hash%1234567891L);
 	}
