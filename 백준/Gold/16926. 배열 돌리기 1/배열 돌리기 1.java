b2e558d37e6f4e2338a9f70cc923c7b775574b5a
kimeloo <149450798+kimeloo@users.noreply.github.com>
2025-02-14 10:45:21 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 480 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 // dR, dC 활용하여 반시계방향 순회
 // 0을 만나거나 표 범위 넘어가면 방향 전환, dir<4까지만
 // 반시계방향으로 돌린 값을 새 2차원 배열에 저장하고, 각 배열을 r칸씩 밀고 다시 표에 대입
@@ -10,29 +13,34 @@ public class Main {
 	static int[] dC = new int[] { 0, 1, 0, -1 };
 	static int[][] map;
 	static ArrayDeque<Integer>[] arr;
-	static Scanner sc;
+	static BufferedReader br;
+	static StringTokenizer st;
 
 	@SuppressWarnings("unchecked")
-	static void input() {
-		N = sc.nextInt();
-		M = sc.nextInt();
-		R = sc.nextInt();
+	static void input() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
 		arr = new ArrayDeque[Math.min(N, M) / 2];
 		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				map[i][j] = sc.nextInt();
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 	}
 
 	static void print() {
+		StringBuilder sb = new StringBuilder();
 		for (int[] line : map) {
 			for (int num : line) {
-				System.out.print(num + " ");
+				sb.append(num).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 
 	static void mapToArr() {
@@ -104,11 +112,11 @@ public class Main {
 		arrToMap();
 	}
 
-	public static void main(String[] args) {
-		sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
 		input();
 		simulate();
 		print();
-		sc.close();
+		br.close();
 	}
 }
