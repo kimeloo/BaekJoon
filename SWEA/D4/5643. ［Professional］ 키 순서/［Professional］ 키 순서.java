3c284fedf1dd7db0a8c2fa0aa19c6ebafbfc307e
kimeloo <149450798+kimeloo@users.noreply.github.com>
2025-02-19 16:22:49 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,901 ms, Memory: 150,472 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,28 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int t, n, m, a, b, cnt, result;
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		HashMap<Integer, ArrayList<Integer>> bigger;
 		HashMap<Integer, ArrayList<Integer>> smaller;
 		ArrayDeque<Integer> q;
 		HashSet<Integer> visited;
-		t = sc.nextInt();
+		st = new StringTokenizer(br.readLine());
+		t = Integer.parseInt(st.nextToken());
 
 		for (int tc = 1; tc <= t; tc++) {
 			bigger = new HashMap<>();
 			smaller = new HashMap<>();
-			n = sc.nextInt();
-			m = sc.nextInt();
+
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			m = Integer.parseInt(st.nextToken());
 			for (int i = 0; i < m; i++) {
-				a = sc.nextInt();
-				b = sc.nextInt();
+				st = new StringTokenizer(br.readLine());
+				a = Integer.parseInt(st.nextToken());
+				b = Integer.parseInt(st.nextToken());
 
 				if (!bigger.containsKey(a)) {
 					bigger.put(a, new ArrayList<>());
@@ -34,13 +43,13 @@ public class Solution {
 				}
 				smaller.get(b).add(a);
 			}
-			
+
 			result = 0;
-			for (int st = 1; st <= n; st++) {
+			for (int student = 1; student <= n; student++) {
 				cnt = 0;
 				q = new ArrayDeque<>();
 				visited = new HashSet<>();
-				q.add(st);
+				q.add(student);
 				while (q.size() > 0) {
 					a = q.poll();
 					if (visited.contains(a)) {
@@ -55,7 +64,7 @@ public class Solution {
 
 				q = new ArrayDeque<>();
 				visited = new HashSet<>();
-				q.add(st);
+				q.add(student);
 				while (q.size() > 0) {
 					a = q.poll();
 					if (visited.contains(a)) {
@@ -67,12 +76,15 @@ public class Solution {
 						q.addAll(smaller.get(a));
 					}
 				}
-				if (cnt==n+1) {
+				if (cnt == n + 1) {
 					result++;
 				}
 			}
 			System.out.printf("#%d %d\n", tc, result);
+
 		}
-		sc.close();
+
+		br.close();
 	}
+
 }
