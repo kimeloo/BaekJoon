32ef2b66f1ea09df71c5b6306c077022bfe4fcd8
kimeloo <149450798+kimeloo@users.noreply.github.com>
2025-03-06 14:29:15 +0900

    [Silver II] Title: 로또, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,44 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int k, r;
+	static int n, r;
 	static int[] pool, selected;
-	static ArrayList<int[]> combResult;
+	static StringBuilder out;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder out = new StringBuilder();
+		out = new StringBuilder();
 
 		r = 6;
+		selected = new int[r];
 		while (true) {
 			st = new StringTokenizer(br.readLine());
-			k = Integer.parseInt(st.nextToken());
-			if (k == 0) {
+			n = Integer.parseInt(st.nextToken());
+			if (n == 0)
 				break;
-			}
 
-			pool = new int[k];
-			for (int i = 0; i < k; i++) {
+			pool = new int[n];
+			for (int i = 0; i < n; i++)
 				pool[i] = Integer.parseInt(st.nextToken());
-			}
 			Arrays.sort(pool);
 
-			combResult = new ArrayList<>();
-			selected = new int[r];
 			comb(0, 0);
 
-			for (int[] combination : combResult) {
-				for (int i : combination) {
-					out.append(i).append(" ");
-				}
-				out.append("\n");
-			}
 			out.append("\n");
 		}
 		System.out.print(out);
@@ -46,18 +36,17 @@ public class Main {
 		br.close();
 	}
 
-	static void comb(int idx, int cnt) {
-		if (cnt == r) {
-			int[] temp = new int[r];
-			System.arraycopy(selected, 0, temp, 0, r);
-			combResult.add(temp);
+	static void comb(int idx, int sidx) {
+		if (sidx == r) {
+			for (int i : selected)
+				out.append(i).append(" ");
+			out.append("\n");
 			return;
 		}
-		if (idx == k) {
+		if (idx == n)
 			return;
-		}
-		selected[cnt] = pool[idx];
-		comb(idx + 1, cnt + 1);
-		comb(idx + 1, cnt);
+		selected[sidx] = pool[idx];
+		comb(idx + 1, sidx + 1);
+		comb(idx + 1, sidx);
 	}
 }
