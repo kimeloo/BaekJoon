12b0912e8fdeb983e6490ecf4c6b0dce692b7859
kimeloo <149450798+kimeloo@users.noreply.github.com>
2025-07-11 15:49:38 +0900

    [Silver I] Title: 스티커, Time: 624 ms, Memory: 101964 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,10 +10,10 @@ public class Main {
 
     static int[][] input() throws IOException {
         N = Integer.parseInt(br.readLine().strip());
-        int[][] result = new int[3][N];
-        for (int i=0; i<2; i++){
+        int[][] result = new int[2][N];
+        for (int i = 0; i < 2; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; j++){
+            for (int j = 0; j < N; j++) {
                 result[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -22,20 +21,22 @@ public class Main {
     }
 
     static int simulate(int[][] dp) {
-        for (int col=1; col<N; col++){
-            for (int i=0; i<2; i++){
-                dp[i][col] += Math.max(dp[1-i][col-1], dp[2][col-1]);
-            }
-            dp[2][col] += Math.max(dp[0][col-1], dp[1][col-1]);
+        if (N > 1) {
+            dp[0][1] += dp[1][0];
+            dp[1][1] += dp[0][0];
         }
-
-        return Math.max(dp[0][N-1], dp[1][N-1]);
+        for (int col = 2; col < N; col++) {
+            for (int i = 0; i < 2; i++) {
+                dp[i][col] += Math.max(dp[1 - i][col - 2], dp[1 - i][col - 1]);
+            }
+        }
+        return Math.max(dp[0][N - 1], dp[1][N - 1]);
     }
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine().strip());
-        for (int tc=0; tc<T; tc++){
+        for (int tc = 0; tc < T; tc++) {
             int[][] inp = input();
             System.out.println(simulate(inp));
         }
