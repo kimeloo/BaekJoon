9b16e3edebabc0f310a3e19f79b27a6c43de61a5
kimeloo <149450798+kimeloo@users.noreply.github.com>
2026-01-01 20:53:11 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 18612 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 
 public class Main {
 
