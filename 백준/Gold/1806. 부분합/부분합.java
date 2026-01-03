32bc343676d58c268193056aedd989a9f695f741
kimeloo <149450798+kimeloo@users.noreply.github.com>
2026-01-03 15:47:53 +0900

    [Gold IV] Title: 부분합, Time: 268 ms, Memory: 25456 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int target;
+    static int[] sumArr;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        int result = bisect();
+        System.out.println(result);
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputStr = br.readLine().trim().split(" ");
+        n = Integer.parseInt(inputStr[0]);
+        target = Integer.parseInt(inputStr[1]);
+        sumArr = new int[n + 1];
+
+        inputStr = br.readLine().trim().split(" ");
+        for (int i = 0; i < n; i++) {
+            int k = Integer.parseInt(inputStr[i]);
+            sumArr[i + 1] = sumArr[i] + k;
+
+        }
+    }
+
+    static int bisect() {
+        // 불가능한 조건
+        if (sumArr[n] < target) {
+            return 0;
+        }
+
+        int lt = 0;
+        int rt = 1;
+        int ans = Integer.MAX_VALUE;
+
+        while (rt <= n) {
+            int sum = sumArr[rt] - sumArr[lt];
+            if (sum < target) {
+                rt++;
+            } else {
+                ans = Math.min(ans, rt - lt);
+                lt++;
+            }
+        }
+        return ans;
+    }
+}
