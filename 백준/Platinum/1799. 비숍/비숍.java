2a2d43136a9b026493609e35c94ef1c096bcb202
kimeloo <149450798+kimeloo@users.noreply.github.com>
2026-01-13 17:45:26 +0900

    [Platinum V] Title: 비숍, Time: 180 ms, Memory: 16612 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.java
+++ 백준/Platinum/1799. 비숍/비숍.java
@@ -1,94 +1,90 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
+import java.util.List;
 
 public class Main {
 
-    static int cnt;
-    static int ans;
     static boolean[][] board;
     static boolean[][] visited;
 
-
     public static void main(String[] args) throws IOException {
         input();
-        solve();
-        System.out.println(ans);
+        int result = solve();
+        System.out.println(result);
     }
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int size = Integer.parseInt(br.readLine());
         board = new boolean[size][size];
-        visited = new boolean[2][2 * board.length - 1];
+        visited = new boolean[2][2 * size - 1];
 
         for (int i = 0; i < size; i++) {
             int[] inputArr = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt).toArray();
             for (int j = 0; j < size; j++) {
-                board[i][j] = inputArr[j] == 0;
+                board[i][j] = inputArr[j] == 1;
             }
         }
     }
 
-    static void solve() {
+    static int solve() {
+        List<int[]> whiteCandidates = collectCandidates(true);
+        List<int[]> blackCandidates = collectCandidates(false);
 
-        Deque<int[]> white = new ArrayDeque<>();
-        Deque<int[]> black = new ArrayDeque<>();
+        int whiteMax = findMaxBishops(whiteCandidates, 0, 0);
+        int blackMax = findMaxBishops(blackCandidates, 0, 0);
 
+        return whiteMax + blackMax;
+    }
+
+    static List<int[]> collectCandidates(boolean isWhite) {
+        List<int[]> candidates = new ArrayList<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j]) {
+                if (!board[i][j]) {
                     continue;
                 }
-                if ((i + j) % 2 == 0) {
-                    white.offer(new int[]{i, j});
-                } else {
-                    black.offer(new int[]{i, j});
+                boolean isWhiteSquare = (i + j) % 2 == 0;
+                if (isWhiteSquare == isWhite) {
+                    candidates.add(new int[]{i, j});
                 }
             }
         }
-
-        ans = Integer.MIN_VALUE;
-        cnt = 0;
-        backtrack(white);
-        int whiteAns = ans;
-
-        ans = Integer.MIN_VALUE;
-        cnt = 0;
-        backtrack(black);
-        ans += whiteAns;
+        return candidates;
     }
 
-    static void backtrack(Deque<int[]> queue) {
-        if (queue.isEmpty()) {
-            ans = Integer.max(cnt, ans);
-            return;
+    static int findMaxBishops(List<int[]> candidates, int index, int currentCount) {
+        if (index == candidates.size()) {
+            return currentCount;
         }
-        int[] next = queue.pollLast();
+
+        int[] position = candidates.get(index);
+        int x = position[0];
+        int y = position[1];
 
         // 비숍을 두지 않고 다음으로 넘어감
-        backtrack(queue);
+        int maxWithoutBishop = findMaxBishops(candidates, index + 1, currentCount);
 
-        if (checkValid(next[0], next[1])) {
-            // 비숍을 두고 다음으로 넘어감
-            reverseValid(next[0], next[1]);
-            cnt++;
-            backtrack(queue);
-            reverseValid(next[0], next[1]);
-            cnt--;
+        // 비숍을 둘 수 있으면 두고 다음으로 넘어감
+        int maxWithBishop = maxWithoutBishop;
+        if (canPlaceBishop(x, y)) {
+            toggleDiagonal(x, y);
+            maxWithBishop = findMaxBishops(candidates, index + 1, currentCount + 1);
+            toggleDiagonal(x, y);
         }
-        queue.offerLast(next);
+
+        return Math.max(maxWithoutBishop, maxWithBishop);
     }
 
-    static boolean checkValid(int x, int y) {
+    static boolean canPlaceBishop(int x, int y) {
         return !(visited[0][x + y] || visited[1][x - y + board.length - 1]);
     }
 
-    static void reverseValid(int x, int y) {
+    static void toggleDiagonal(int x, int y) {
         visited[0][x + y] = !visited[0][x + y];
         visited[1][x - y + board.length - 1] = !visited[1][x - y + board.length - 1];
     }
