3d54dd9ddd8c9fec3f39719f7be4d1ef36707623
kimeloo <149450798+kimeloo@users.noreply.github.com>
2026-01-19 18:22:14 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 600 ms, Memory: 139420 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -22,12 +22,12 @@ public class Main {
 
     static int solve(int[] arr) {
         List<Integer> dp = new ArrayList<>();
-        dp.add(Integer.MAX_VALUE);
         for (int n : arr) {
-            if (dp.get(dp.size() - 1) >= n) {
-                dp.set(findIdx(dp, n), n);
-            } else {
+            int idx = findIdx(dp, n);
+            if (idx == dp.size()) {
                 dp.add(n);
+            } else {
+                dp.set(idx, n);
             }
         }
         return dp.size();
@@ -35,7 +35,7 @@ public class Main {
 
     static int findIdx(List<Integer> list, int n) {
         int lt = 0;
-        int rt = list.size() - 1;
+        int rt = list.size();
 
         while (lt < rt) {
             int mid = lt + (rt - lt) / 2;
@@ -44,9 +44,6 @@ public class Main {
             } else {
                 lt = mid + 1;
             }
-        }
-        if (list.get(lt) < n) {
-            return lt + 1;
         }
         return lt;
     }
