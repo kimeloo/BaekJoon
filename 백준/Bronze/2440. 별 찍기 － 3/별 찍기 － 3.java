cf9c3d531c0a4fe9653313ca5281d556b35cd79f
kimeloo <149450798+kimeloo@users.noreply.github.com>
2026-01-26 18:34:29 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 112 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder out = new StringBuilder();
+        for (int i = n; i > 0; i--) {
+            out.append("*".repeat(i)).append("\n");
+        }
+        System.out.println(out);
+    }
+}
